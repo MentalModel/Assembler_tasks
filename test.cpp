74f749976e918e07526337317c7e0e79856bf099
MentalModel <alexander@dobrunov.ru>
2015-07-13 22:30:50 +0500

    give a personal hello

--- test.cpp
+++ test.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	cout<<"test\n";
-	return 0;
+    char name[255];
+
+    printf("Enter your name: ");
+    fgets(name, 255, stdin);
+    name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+
+    printf("Hello %s!\n", name);
+    return 0;
 }
